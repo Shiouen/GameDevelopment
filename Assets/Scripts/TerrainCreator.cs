43782e5f7ac83b9db22429ee0e4627ad0b70d6bb
benniehelsen <helsen.bennie@hotmail.be>
2015-11-24 15:29:52 +0100

    Added walls, fl, floor, player and movement, textures

---
+++ Assets/Scripts/TerrainCreator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainCreator : MonoBehaviour {
+	public GameObject wallPrefab;
+
+	// Use this for initialization
+	void Start () {
+		this.wallPrefab.transform.position = new Vector3(50,5,90);
+		this.wallPrefab.transform.localScale = new Vector3(70,10,2);
+		GameObject wall = (GameObject)Instantiate (wallPrefab, this.wallPrefab.transform.position, Quaternion.Euler(0,90,0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
